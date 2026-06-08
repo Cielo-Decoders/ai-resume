a5e81e7207458e8e7d748bd1232eb3570e0b9acd
isaacnarteh <nartehi@berea.edu>
2026-06-07 22:09:33 -0500

    Revert "Merge pull request #55 from Cielo-Decoders/update_resume"
    This reverts commit 13362ded1276d5a17924dcfcee8a748555bb180c, reversing
    changes made to 3232cdaa7e88b31612631e63c05664ae6ab92d52.

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
   const navigate = useNavigate();
 
   const handleGetStarted = () => {
-    navigate('/get-started');
+    navigate('/app');
   };
 
   const iconFeatures = [
